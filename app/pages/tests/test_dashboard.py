89b77e31e7013d569362db5ac73e8f1e6b7d0498
J-Y-P-tech <jordan.yurukov@gmail.com>
2023-07-05 15:46:36 +0300

    Added page Dashboard Implemented Filter functionality Imlpemented Sort functionality

--- app/pages/tests/test_dashboard.py
+++ app/pages/tests/test_dashboard.py
@@ -1,6 +1,9 @@
 from django.urls import reverse
 from django.test import SimpleTestCase, TestCase, Client
 from django.contrib.auth.models import User
+from core.models import Stock
+from decimal import Decimal
+from bs4 import BeautifulSoup
 
 
 class TestDashboardPrivate(TestCase):
@@ -28,13 +31,13 @@ class TestDashboardPrivate(TestCase):
         # Register user for the test
         self.client = Client()
         self.client.post(reverse('register'),
-                                    {
-                                        'first_name': '1',
-                                        'last_name': '1',
-                                        'email': '1@example.com',
-                                        'password': '11111',
-                                        'confirm_password': '11111'
-                                    })
+                         {
+                             'first_name': '1',
+                             'last_name': '1',
+                             'email': '1@example.com',
+                             'password': '11111',
+                             'confirm_password': '11111'
+                         })
 
         # Login registered user
         self.client.login(username='1@example.com', password='1111')
@@ -44,3 +47,275 @@ class TestDashboardPrivate(TestCase):
 
         # Confirm response 200 - Success
         self.assertEqual(response.status_code, 200)
+
+
+class TestDashboardSearch(TestCase):
+    """
+    Test for search on page Dashboard
+    """
+
+    def setUp(self):
+        """
+        Register user for the tests
+        """
+        self.client = Client()
+        self.client.post(reverse('register'),
+                         {
+                             'first_name': '1',
+                             'last_name': '1',
+                             'email': '1@example.com',
+                             'password': '11111',
+                             'confirm_password': '11111'
+                         })
+
+        # Login registered user
+        self.client.login(username='1@example.com', password='1111')
+
+    def test_dashboard_search_return_values(self):
+        """
+        Confirm that POST request returns correct values for search variables
+        """
+        # Generate Search with default values
+        data = {
+            'fa_score': 30,
+            'rsi': 40,
+            'avg_gain_loss': 10,
+            'five_year_avg_dividend_yield': 2
+        }
+        # send a post request to the form view with the data
+        response = self.client.post(reverse('dashboard'), data)
+
+        # assert that the response status code is 200 (OK)
+        self.assertEqual(response.status_code, 200)
+
+        # Assert that response values match tha data from POST request
+        self.assertEqual(response.context.get('fa_score'), str(data['fa_score']))
+        self.assertEqual(response.context.get('rsi'), str(data['rsi']))
+        self.assertEqual(response.context.get('avg_gain_loss'), str(data['avg_gain_loss']))
+        self.assertEqual(response.context.get('five_year_avg_dividend_yield'),
+                         str(data['five_year_avg_dividend_yield']))
+
+
+class TestDashboardFilter(TestCase):
+    """
+    Test class for filter option on page Dashboard
+    """
+
+    def setUp(self):
+        """
+        Register user for the tests
+        Create test data in model Stock
+        """
+        self.client = Client()
+        self.client.post(reverse('register'),
+                         {
+                             'first_name': '1',
+                             'last_name': '1',
+                             'email': '1@example.com',
+                             'password': '11111',
+                             'confirm_password': '11111'
+                         })
+
+        # Login registered user
+        self.client.login(username='1@example.com', password='1111')
+
+        Stock.objects.create(
+            stock_code='ABC',
+            sector='Technology',
+            industry='Software',
+            country='USA',
+            exchange_short_name='NYSE',
+            company_name='ABC Inc.',
+            rsi=37,
+            fa_score=30,
+            avg_gain_loss=5,
+            five_year_avg_dividend_yield=3
+        )
+        Stock.objects.create(
+            stock_code='XYZ',
+            sector='Finance',
+            industry='Banking',
+            country='UK',
+            exchange_short_name='LSE',
+            company_name='XYZ Bank',
+            rsi=39,
+            fa_score=35,
+            avg_gain_loss=12,
+            five_year_avg_dividend_yield=2
+        )
+
+    def test_dashboard_filter_avg_gain_loss(self):
+        """
+        Test that filtering functionality works for avg_gain_loss
+        Test will perform search for specific avg-gain_loss and only
+        one company should be returned
+        """
+
+        # Send a POST request with filter values
+        data = {
+            'fa_score': 20,
+            'rsi': 40,
+            'avg_gain_loss': 10,
+            'five_year_avg_dividend_yield': 1
+        }
+        # send a post request to the form view with the data
+        response = self.client.post(reverse('dashboard'), data)
+
+        # Assert that the response has a successful status code
+        self.assertEqual(response.status_code, 200)
+        # print(response.context)
+
+        # Assert that the filtered stocks are displayed in the table
+        self.assertContains(response, 'XYZ')
+        self.assertNotContains(response, 'ABC')
+
+    def test_dashboard_filter_fa_score(self):
+        """
+        Test filter on page Dashboard for fa_score
+        """
+        # Send a POST request with filter values
+        data = {
+            'rsi': 40,
+            'fa_score': 30,
+            'avg_gain_loss': 4,
+            'five_year_avg_dividend_yield': 1
+        }
+
+        # Send POST request to page Dashboard with data as context
+        response = self.client.post(reverse('dashboard'), data)
+
+        # Confirm success code 200
+        self.assertEqual(response.status_code, 200)
+
+        # Assert that XYZ is displayed and ABC is not
+        self.assertContains(response, 'XYZ')
+        self.assertNotContains(response, 'ABC')
+
+    def test_dashboard_filter_rsi(self):
+        """
+        Test filter on page Dashboard for rsi
+        """
+        # Send a POST request with filter values
+        data = {
+            'rsi': 38,
+            'fa_score': 25,
+            'avg_gain_loss': 4,
+            'five_year_avg_dividend_yield': 1
+        }
+
+        # Send POST request to page Dashboard with data as context
+        response = self.client.post(reverse('dashboard'), data)
+
+        # Confirm success code 200
+        self.assertEqual(response.status_code, 200)
+
+        # Assert that company 'ABC' is part of the result and 'XYZ' is not
+        self.assertContains(response, 'ABC')
+        self.assertNotContains(response, 'XYZ')
+
+    def test_dashboard_filter_five_year_avg_dividend_yield(self):
+        """
+        Test filter on page Dashboard for filter_five_year_avg_dividend_yield
+        """
+        # Send a POST request with filter values
+        data = {
+            'rsi': 40,
+            'fa_score': 25,
+            'avg_gain_loss': 4,
+            'five_year_avg_dividend_yield': 2
+        }
+
+        # Send POST request to page Dashboard
+        response = self.client.post(reverse('dashboard'), data)
+
+        # Confirm success code 200
+        self.assertEqual(response.status_code, 200)
+
+        # Confirm that company 'ABC' is part of the result and 'XYZ' is not
+        self.assertContains(response, 'ABC')
+        self.assertNotContains(response, 'XYZ')
+
+
+class TestDashboardSort(TestCase):
+    """
+    Test sort functionality on page Dashboard
+    """
+
+    def setUp(self):
+        self.client = Client()
+        self.client.post(reverse('register'),
+                         {
+                             'first_name': '1',
+                             'last_name': '1',
+                             'email': '1@example.com',
+                             'password': '11111',
+                             'confirm_password': '11111'
+                         })
+
+        # Login registered user
+        self.client.login(username='1@example.com', password='1111')
+        # Create sample Stock objects for testing
+        Stock.objects.create(stock_code='ABC', sector='Technology', industry='Software',
+                             country='USA', exchange_short_name='NASDAQ', company_name='ABC Inc.',
+                             rsi=50, fa_score=80, avg_gain_loss=Decimal('10.5'),
+                             five_year_avg_dividend_yield=Decimal('2.3'))
+        Stock.objects.create(stock_code='DEF', sector='Finance', industry='Banking',
+                             country='UK', exchange_short_name='LSE', company_name='DEF Bank',
+                             rsi=60, fa_score=70, avg_gain_loss=Decimal('11.2'),
+                             five_year_avg_dividend_yield=Decimal('1.8'))
+
+    @staticmethod
+    def get_table_rows_order(response):
+        soup = BeautifulSoup(response.content, 'html.parser')
+        rows = soup.select('table tbody tr')
+        actual_order = [row.find('td').text for row in rows]
+
+        return actual_order
+
+    def test_sort_by_fa_score(self):
+        """
+        Test sort by fa_score value. The stock with less fa_score should be first
+        """
+        form_data = {'sort': 'fa_score'}
+        response = self.client.post(reverse('dashboard'), data=form_data)
+        actual_order = self.get_table_rows_order(response)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(actual_order, ['DEF', 'ABC'])
+
+    def test_sort_by_rsi(self):
+        """
+        Test sort by RSI. Simulate POST request like pressing RSI.
+        Rows should be ordered by RSI in ascending order.
+        """
+        form_data = {'sort': 'rsi'}
+        response = self.client.post(reverse('dashboard'), data=form_data)
+        actual_order = self.get_table_rows_order(response)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(actual_order, ['ABC', 'DEF'])
+
+    def test_sort_by_avg_gain_loss(self):
+        """
+        Test sort by avg_gain_loss. Simulate POST request like pressing avg_gain_loss.
+        Rows should be ordered by avg_gain_loss in ascending order.
+        """
+        form_data = {'sort': 'avg_gain_loss'}
+        response = self.client.post(reverse('dashboard'), form_data)
+        actual_order = self.get_table_rows_order(response)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(actual_order, ['ABC', 'DEF'])
+
+    def test_by_five_year_avg_dividend_yield(self):
+        """
+        Test sort by five_year_avg_dividend_yield. Simulate POST request by pressing
+        five_year_avg_dividend_yield button.
+        Rows should be ordered by five_year_avg_dividend_yield in ascending order
+        """
+        form_data = {'sort': 'five_year_avg_dividend_yield'}
+        response = self.client.post(reverse('dashboard'), form_data)
+        actual_order = self.get_table_rows_order(response)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(actual_order, ['DEF', 'ABC'])
--- app/pages/views.py
+++ app/pages/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
+from core.models import Stock
+import decimal
 
 
 def home(request):
@@ -9,4 +11,105 @@ def home(request):
 
 @login_required(login_url='/accounts/login')
 def dashboard(request):
-    return render(request, 'pages/dashboard.html')
+    """
+    View that handles dashboard page
+    """
+    if request.method == 'POST':
+        fa_score = request.POST.get('fa_score')
+        rsi = request.POST.get('rsi')
+        avg_gain_loss = request.POST.get('avg_gain_loss')
+        five_year_avg_dividend_yield = request.POST.get('five_year_avg_dividend_yield')
+        sort_field = request.POST.get('sort')  # Get the sort field from the clicked button
+
+        all_stocks = Stock.objects.all()
+
+        # Filter
+        # fa_score greater than
+        if fa_score:
+            all_stocks = all_stocks.filter(fa_score__gt=int(fa_score))
+        # rsi less than
+        if rsi:
+            all_stocks = all_stocks.filter(rsi__lte=int(rsi))
+        # avg_gain_loss greater than
+        if avg_gain_loss:
+            all_stocks = all_stocks.filter(avg_gain_loss__gt=decimal.Decimal(avg_gain_loss))
+        # five_year_dividend_yield greater than
+        if five_year_avg_dividend_yield:
+            all_stocks = all_stocks.filter(
+                five_year_avg_dividend_yield__gt=decimal.Decimal(five_year_avg_dividend_yield))
+
+        # Retrieve the current sort direction from session or set it to ascending by default
+        sort_direction = request.session.get('sort_direction', 'ascending')
+
+        # Sort
+        if sort_field:
+            if sort_field == request.session.get('sort_field'):
+                # If the same field is clicked again, reverse the sort direction
+                sort_direction = 'ascending' if sort_direction == 'descending' else 'descending'
+            else:
+                # If a different field is clicked, set the sort direction to ascending
+                sort_direction = 'ascending'
+
+            request.session['sort_direction'] = sort_direction
+            request.session['sort_field'] = sort_field
+
+            # Determine the prefix '-' for descending order
+            sort_prefix = '-' if sort_direction == 'descending' else ''
+
+            # Sort the queryset based on the selected field and direction
+            sort_field_with_prefix = f'{sort_prefix}{sort_field}'
+            all_stocks = all_stocks.order_by(sort_field_with_prefix)
+
+        # Set default values if none are provided
+        data = {
+            'fa_score': fa_score,
+            'rsi': rsi,
+            'avg_gain_loss': avg_gain_loss,
+            'five_year_avg_dividend_yield': five_year_avg_dividend_yield,
+            'all_stocks': all_stocks,
+        }
+
+        return render(request, 'pages/dashboard.html', data)
+
+    if request.method == 'GET':
+        fa_score = request.GET.get('fa_score')
+        rsi = request.GET.get('rsi')
+        avg_gain_loss = request.GET.get('avg_gain_loss')
+        five_year_avg_dividend_yield = request.GET.get('five_year_avg_dividend_yield')
+
+        if not fa_score:
+            fa_score = 30
+        if not rsi:
+            rsi = 40
+        if not avg_gain_loss:
+            avg_gain_loss = 10
+        if not five_year_avg_dividend_yield:
+            five_year_avg_dividend_yield = 1
+
+        all_stocks = Stock.objects.all()
+
+        # Filter
+        # fa_score greater than
+        if fa_score:
+            all_stocks = all_stocks.filter(fa_score__gt=int(fa_score))
+        # rsi less than
+        if rsi:
+            all_stocks = all_stocks.filter(rsi__lte=int(rsi))
+        # avg_gain_loss greater than
+        if avg_gain_loss:
+            all_stocks = all_stocks.filter(avg_gain_loss__gt=decimal.Decimal(avg_gain_loss))
+        # five_year_dividend_yield greater than
+        if five_year_avg_dividend_yield:
+            all_stocks = all_stocks.filter(
+                five_year_avg_dividend_yield__gt=decimal.Decimal(five_year_avg_dividend_yield))
+
+            # Set default values if none are provided
+        data = {
+            'fa_score': fa_score,
+            'rsi': rsi,
+            'avg_gain_loss': avg_gain_loss,
+            'five_year_avg_dividend_yield': five_year_avg_dividend_yield,
+            'all_stocks': all_stocks,
+        }
+
+        return render(request, 'pages/dashboard.html', data)
